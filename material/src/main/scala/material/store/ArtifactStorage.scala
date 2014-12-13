2cbe9eac1b1aba1df00e863007641eadfbdb728a
Sriram <sriram@indix.com>
2014-12-13 12:54:25 +0530

    First drop for S3 Material Plugin - S3 Artifact Store interface

---
+++ material/src/main/scala/material/store/ArtifactStorage.scala
@@ -0,0 +1,19 @@
+package material.store
+
+import org.apache.hadoop.fs.FileSystem
+import org.apache.hadoop.conf.Configuration
+
+
+sealed trait ArtifactStore {
+  def get(from: String, to: String): Unit
+  def put(from: String, to: String): Unit
+  def latest(materialName: String): String
+  def deltaAfter(lastUpdated: Long): List[String]
+}
+
+case class S3ArtifactStore(conf: Configuration = new Configuration()) extends ArtifactStore with FileHelper {
+  override def get(from: String, to: String) = copyToLocal(from, to, conf)
+  override def put(from: String, to: String) = copyFromLocal(from, to, conf)
+  override def latest(materialName: String): String = ???
+  override def deltaAfter(lastUpdated: Long): List[String] = ???
+}
