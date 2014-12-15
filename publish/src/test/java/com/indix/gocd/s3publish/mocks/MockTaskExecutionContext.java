a0fde7e10210337763face5076067e3b48ad389c
Ashwanth Kumar <ashwanthkumar@googlemail.com>
2014-12-15 16:36:49 +0530

    Adding tests for PublishExecutor

---
+++ publish/src/test/java/com/indix/gocd/s3publish/mocks/MockTaskExecutionContext.java
@@ -0,0 +1,68 @@
+package com.indix.gocd.s3publish.mocks;
+
+import com.thoughtworks.go.plugin.api.task.Console;
+import com.thoughtworks.go.plugin.api.task.EnvironmentVariables;
+import com.thoughtworks.go.plugin.api.task.TaskExecutionContext;
+
+import java.io.InputStream;
+import java.util.Map;
+
+public class MockTaskExecutionContext implements TaskExecutionContext {
+    private Map<String, String> mockEnvironmentVariables;
+    private String workingDir;
+
+    public MockTaskExecutionContext(Map<String, String> mockEnvironmentVariables, String workingDir) {
+        this.mockEnvironmentVariables = mockEnvironmentVariables;
+        this.workingDir = workingDir;
+    }
+
+    @Override
+    public EnvironmentVariables environment() {
+        return new EnvironmentVariables() {
+            @Override
+            public Map<String, String> asMap() {
+                return mockEnvironmentVariables;
+            }
+
+            @Override
+            public void writeTo(Console console) {
+            }
+
+            @Override
+            public Console.SecureEnvVarSpecifier secureEnvSpecifier() {
+                return new Console.SecureEnvVarSpecifier() {
+                    @Override
+                    public boolean isSecure(String s) {
+                        return false;
+                    }
+                };
+            }
+        };
+    }
+
+    @Override
+    public Console console() {
+        return new Console() {
+            @Override
+            public void printLine(String s) {
+            }
+
+            @Override
+            public void readErrorOf(InputStream inputStream) {
+            }
+
+            @Override
+            public void readOutputOf(InputStream inputStream) {
+            }
+
+            @Override
+            public void printEnvironment(Map<String, String> stringStringMap, SecureEnvVarSpecifier secureEnvVarSpecifier) {
+            }
+        };
+    }
+
+    @Override
+    public String workingDir() {
+        return null;
+    }
+}
