4c3267499ab26ac99bb3e6ecf67c30d8e4512c10
haimizrael <hizrael@gcmlp.com>
2015-12-13 17:30:43 -0600

    Add logging to AWSCredentialsFactory

--- utils/src/main/java/com/indix/gocd/utils/AWSCredentialsFactory.java
+++ utils/src/main/java/com/indix/gocd/utils/AWSCredentialsFactory.java
@@ -1,6 +1,7 @@
 package com.indix.gocd.utils;
 
 import com.amazonaws.auth.*;
+import com.thoughtworks.go.plugin.api.logging.Logger;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -14,6 +15,7 @@ public class AWSCredentialsFactory {
     private static final List<String> validUseInstanceProfileValues = new ArrayList<String>(Arrays.asList("true","false","yes","no","1","0"));
     private static final List<String> affirmativeUseInstanceProfileValues = new ArrayList<String>(Arrays.asList("true","yes","1"));
     private GoEnvironment env;
+    private Logger log = Logger.getLoggerFor(AWSCredentialsFactory.class);
 
     public AWSCredentialsFactory(GoEnvironment goEnvironment) {
         this.env = goEnvironment;
@@ -22,11 +24,15 @@ public class AWSCredentialsFactory {
     public AWSCredentialsProvider getCredentialsProvider() {
         List<AWSCredentialsProvider> providers = new ArrayList<AWSCredentialsProvider>();
         if (!env.isAbsent(AWS_USE_INSTANCE_PROFILE)) {
-            if (affirmativeUseInstanceProfileValues.contains(env.get(AWS_USE_INSTANCE_PROFILE).toLowerCase())) {
+            String useInstanceProfileCode = env.get(AWS_USE_INSTANCE_PROFILE);
+            if (affirmativeUseInstanceProfileValues.contains(useInstanceProfileCode.toLowerCase())) {
+                log.debug(String.format(
+                        "AWS_USE_INSTANCE_PROFILE=%s;Initializing with InstanceProfileCredentialsProvider",
+                        useInstanceProfileCode));
                 providers.add(new InstanceProfileCredentialsProvider());
             }
-            else if (!validUseInstanceProfileValues.contains(env.get(AWS_USE_INSTANCE_PROFILE).toLowerCase())) {
-                throwEnvInvalidFormat(AWS_USE_INSTANCE_PROFILE, env.get(AWS_USE_INSTANCE_PROFILE),
+            else if (!validUseInstanceProfileValues.contains(useInstanceProfileCode.toLowerCase())) {
+                throwEnvInvalidFormat(AWS_USE_INSTANCE_PROFILE, useInstanceProfileCode,
                         validUseInstanceProfileValues.toString());
             }
         }
@@ -38,6 +44,7 @@ public class AWSCredentialsFactory {
                 throwEnvNotFoundIllegalArgumentException(AWS_SECRET_ACCESS_KEY);
 
             // See AccessKeyCredentialsProvider as to why use it instead of built-in EnvironmentVariablesCredentialsProvider
+            log.debug("AWS_ACCESS_KEY_ID/AWS_SECRET_ACCESS_KEY are present;Initializing with AccessKeyCredentialsProvider");
             providers.add(new AccessKeyCredentialsProvider(
                     env.get(AWS_ACCESS_KEY_ID),
                     env.get(AWS_SECRET_ACCESS_KEY)
