688fa1d06a0598f49caaa0c6fa20cd4dd549118f
Manoj <manojlds@gmail.com>
2017-05-09 23:58:00 +0530

    Use yarn for docs

--- docs/deploy.sh
+++ docs/deploy.sh
@@ -1,21 +1,6 @@
 #!/bin/bash
 
-set -e
+set -ex
 
-npm install
-npm run docs:build
-
-DEPLOY_FOLDER="deploy/"
-
-if [ ! -d "$DEPLOY_FOLDER" ]; then
-  git clone -b gh-pages git@github.com:indix/gocd-s3-artifacts $DEPLOY_FOLDER
-fi
-
-cd $DEPLOY_FOLDER
-git pull origin gh-pages
-rm -rf *
-cp -r ../_book/* .
-
-git add -A
-git commit -am "Site generated at $(date)"
-# git push origin gh-pages
+yarn install
+yarn run docs:build
